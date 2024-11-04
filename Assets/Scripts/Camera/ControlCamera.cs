77d988b6ac2a8b8118adf7b5ff256acfa93bb844
Pol Blesa <pol.blesa@enti.cat>
2024-11-04 19:36:42 +0100

    CameraFollowingPlayer

---
+++ Assets/Scripts/Camera/ControlCamera.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class ControlCamara : MonoBehaviour
+{
+    private GameObject player;
+    public Vector3 distancia;
+
+    void Update()
+    {
+        this.player = GameObject.Find("Player");
+        this.distancia = new Vector3(0f, 0f, 5f);
+    }
+    void LateUpdate()
+    {
+        gameObject.transform.position = this.player.transform.position - this.distancia;
+    }
+}
