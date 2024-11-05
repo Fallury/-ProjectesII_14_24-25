5f8b9fffd801c07c3ee7d73ff45c10bb536afc63
Pol Blesa <pol.blesa@enti.cat>
2024-11-06 00:54:37 +0100

    Mejora del Seguimiento del enemigo hacia el Player

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -12,14 +12,21 @@ public class EnemyMovement : MonoBehaviour
     [SerializeField] private Transform Player;
 
     private void Update()
-    {        
-        if (Vector2.Distance(transform.position, Player.position) > minDistanceToPlayer)     
-        { 
-            transform.position = Vector2.MoveTowards(transform.position, TowerDefence.position, speed * Time.deltaTime);    
-        }        
-        else  
-        {  
-            transform.position = Vector2.MoveTowards(transform.position, Player.position, speed * Time.deltaTime);
-        } 
+    {
+        if (Vector2.Distance(transform.position, TowerDefence.position) > minDistanceToTowerrDefence)
+        {
+            if (Vector2.Distance(transform.position, Player.position) > minDistanceToPlayer)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, TowerDefence.position, speed * Time.deltaTime);
+            }
+            else
+            {
+                transform.position = Vector2.MoveTowards(transform.position, Player.position, speed * Time.deltaTime);
+            }
+        }
+        else
+        {       
+            transform.position = Vector2.MoveTowards(transform.position, TowerDefence.position, speed * Time.deltaTime); 
+        }
     }
 }
