043af7cb620d415400053dc49962cbb9b94f8bb1
Pol Blesa <pol.blesa@enti.cat>
2024-11-06 00:46:37 +0100

    SeguimientoDelEnemigoAlPlayer

---
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Rendering;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float minDistanceToPlayer;
+    [SerializeField] private float minDistanceToTowerrDefence;
+    [SerializeField] private Transform TowerDefence;
+    [SerializeField] private Transform Player;
+
+    private void Update()
+    {        
+        if (Vector2.Distance(transform.position, Player.position) > minDistanceToPlayer)     
+        { 
+            transform.position = Vector2.MoveTowards(transform.position, TowerDefence.position, speed * Time.deltaTime);    
+        }        
+        else  
+        {  
+            transform.position = Vector2.MoveTowards(transform.position, Player.position, speed * Time.deltaTime);
+        } 
+    }
+}
