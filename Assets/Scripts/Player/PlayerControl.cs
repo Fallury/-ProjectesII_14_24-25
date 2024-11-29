ab853922b43dc5ee016b12a6aec073c4074d1a0b
Pol Blesa <pol.blesa@enti.cat>
2024-11-29 15:48:31 +0100

    Prototipo Animación Personaje

--- Assets/Scripts/Player/PlayerControl.cs
+++ Assets/Scripts/Player/PlayerControl.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
@@ -7,10 +8,14 @@ public class PlayerControl : MonoBehaviour
     [SerializeField] private float speed;
     private Rigidbody2D playerRb;
     private Vector2 moveInput;
+    private Animator myAnimator;
+    private SpriteRenderer mySpriteRender;
 
     void Start()
     {
         playerRb = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        mySpriteRender = GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -18,10 +23,29 @@ public class PlayerControl : MonoBehaviour
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
         moveInput = new Vector2(moveX, moveY).normalized;
+
+        myAnimator.SetFloat("moveX", moveX);
+        myAnimator.SetFloat("moveY", moveY);
     }
 
     private void FixedUpdate()
     {
+        AdjustPlayerFacingDirection();
         playerRb.MovePosition(playerRb.position + moveInput * speed * Time.fixedDeltaTime);
+    }
+
+    private void AdjustPlayerFacingDirection()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(transform.position);
+
+        if(mousePos.x < playerScreenPoint.x)
+        {
+            mySpriteRender.flipX = true;
+        }
+        else
+        {
+            mySpriteRender.flipX = false;
+        }
     }
 }
